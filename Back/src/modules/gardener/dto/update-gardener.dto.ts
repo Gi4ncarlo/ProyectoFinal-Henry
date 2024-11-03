3975d20bb7af24c27a61733ae35e182fc7b45b32
Giancarlo <giancarlozanarini9@gmail.com>
2024-11-03 14:10:02 -0300

    Completo DTO Gardener

--- Back/src/modules/gardener/dto/update-gardener.dto.ts
+++ Back/src/modules/gardener/dto/update-gardener.dto.ts
@@ -1,4 +1,56 @@
-import { PartialType } from '@nestjs/swagger';
-import { CreateGardenerDto } from './create-gardener.dto';
-
-export class UpdateGardenerDto extends PartialType(CreateGardenerDto) {}
+import {
+    IsOptional,
+    IsString,
+    IsInt,
+    IsNumber,
+    IsUrl,
+    Min,
+    Max,
+    IsEmail,
+  } from 'class-validator';
+  
+  export class UpdateGardenerDto {
+    @IsString()
+    @IsOptional()
+    name?: string;
+  
+    @IsEmail()
+    @IsOptional()
+    email?: string;
+  
+    @IsString()
+    @IsOptional()
+    password?: string;
+  
+    @IsInt()
+    @IsOptional()
+    age?: number;
+  
+    @IsString()
+    @IsOptional()
+    phone?: string;
+  
+    @IsString()
+    @IsOptional()
+    experience?: string;
+  
+    @IsInt()
+    @Min(1)
+    @Max(5)
+    @IsOptional()
+    calification?: number;
+  
+    @IsString()
+    @IsOptional()
+    ubication?: string;
+  
+    @IsNumber()
+    @Min(0)
+    @IsOptional()
+    costPerHour?: number;
+  
+    @IsUrl()
+    @IsOptional()
+    profileImageUrl?: string;
+  }
+  
