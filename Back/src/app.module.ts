ac7045fe1b877cbc30d4b57caae9eb0ed7d4911d
Lucas Badino <lucas.badino.7@gmail.com>
2024-11-04 19:28:54 -0300

    fix

--- Back/src/app.module.ts
+++ Back/src/app.module.ts
@@ -3,6 +3,7 @@ import { dataSource } from './config/data-sorce';
 import { ServiceDetailsModule } from './modules/service-details/service-details.module';
 import { ServiceProvidedModule } from './modules/serviceProvided/serviceProvided.module';
 
+
 @Module({
   imports: [
     dataSource,
