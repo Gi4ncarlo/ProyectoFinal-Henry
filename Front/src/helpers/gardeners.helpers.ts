273f12f14d4621568c476003c53a22bad926863d
gtgretatarabelli <150402785+gtgretatarabelli@users.noreply.github.com>
2024-11-10 12:39:26 +0100

    feat: :zap: Creacion Ventana Gardenes
    Cree la ventana GARDENERS, para que se muestren todas las tarjetas de los jardineros con su respectivo boton para poder contactarlos.

--- Front/src/helpers/gardeners.helpers.ts
+++ Front/src/helpers/gardeners.helpers.ts
@@ -1,31 +1,30 @@
+import { IServiceProvider } from "@/interfaces/IServiceProvider";
 
 
 const APIURL = process.env.NEXT_PUBLIC_API_URL
 
-  export async function getGardenersDB() {
-    try {
-        const res = await fetch(`${APIURL}/gardener`, {
-          mode: 'no-cors',
-          method: 'GET',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-        });
 
+export async function getGardenersDB(): Promise<IServiceProvider[]> {
+    try {
+      const res = await fetch(`${APIURL}/gardener`, {
+        next: { revalidate: 1200 },
+      });
+  
       if (!res.ok) {
-        throw new Error(`Error en la solicitud: ${res.status}`);
-      }
-      const textResponse = await res.text();
-      if (!textResponse.trim()) {
-        throw new Error('La respuesta está vacía');
+        throw new Error("Failed to fetch gardeners");
       }
   
-      const gardeners = JSON.parse(textResponse);
-      return gardeners;
-  
-    } catch (error) {
+      const response = await res.json();
+      console.log(response);
+      // Verifica que la propiedad `data` esté presente y sea un arreglo
+      if (Array.isArray(response.data)) {
+        return response.data;  // Accede a la propiedad `data`
+      } else {
+        throw new Error("Invalid data format, expected an array in 'data'");
+      }
+    } catch (error: any) {
       console.error("Error fetching gardeners:", error);
-      throw new Error('Error al cargar los jardineros');
+      return [];  // Devuelve un arreglo vacío en caso de error
     }
   }
   
