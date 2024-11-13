bf996afcb208b8ba1fe1bdc62dcfcdf63a6dd181
Pablo <olbap_olimpo@hotmail.com>
2024-11-13 11:54:06 -0300

    a

---
+++ Front/src/app/gardener/page.tsx
@@ -0,0 +1,13 @@
+
+import ProviderCardList from '@/components/ProviderCardList/ProviderCardList';
+import React from 'react'
+
+const gardener = () => {
+  return (
+    <div className="flex mt-24">
+<ProviderCardList/>
+    </div>
+  )
+}
+
+export default gardener;
