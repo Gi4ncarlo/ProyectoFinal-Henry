51a4a3bfa575618854a0ac321e8b4d7061062786
Giancarlo <giancarlozanarini9@gmail.com>
2024-11-16 23:23:17 -0300

    Intento 2 de deploy con union Main existente/Nacho/Mauri/Gian/Lucas

--- Back/src/modules/auth/auth.service.ts
+++ Back/src/modules/auth/auth.service.ts
@@ -1,4 +1,9 @@
-import { BadRequestException, HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  HttpException,
+  HttpStatus,
+  Injectable,
+} from '@nestjs/common';
 import { User } from '../user/entities/user.entity';
 import { SignInAuthDto } from '../user/dto/signin-user.dto';
 import { UserService } from '../user/user.service';
@@ -7,133 +12,133 @@ import { SignUpAuthDto } from '../user/dto/signup-user.dto';
 import * as bcrypt from 'bcrypt';
 import { AdminService } from '../admin/admin.service';
 import { GardenerService } from '../gardener/gardener.service';
-<<<<<<< HEAD
 import { Role } from '../user/enums/role.enum';
-=======
 import { MailService } from '../mail/mail.service';
->>>>>>> 5629a5329a662a1d895d34dc854da596e1fb750c
 
 @Injectable()
 export class AuthService {
-    constructor(
-        private userService: UserService,
-        private adminService: AdminService,
-        private gardenerService: GardenerService,
-<<<<<<< HEAD
-        private jwtService: JwtService
-    ) {}
-=======
-        private jwtService: JwtService,
-        private readonly mailService: MailService,
-    ) { }
->>>>>>> 5629a5329a662a1d895d34dc854da596e1fb750c
+  constructor(
+    private userService: UserService,
+    private adminService: AdminService,
+    private gardenerService: GardenerService,
+    private jwtService: JwtService,
+    private readonly mailService: MailService,
+  ) {}
 
-    async signIn(credentials: SignInAuthDto) {
-        try {
-            console.log("Credenciales recibidas en signIn:", credentials);
+  async signIn(credentials: SignInAuthDto) {
+    try {
+      console.log('Credenciales recibidas en signIn:', credentials);
 
-            const user = await this.userService.findByEmail(credentials.email);
-            const admin = await this.adminService.findByEmail(credentials.email);
-            const gardener = await this.gardenerService.findByEmail(credentials.email);
+      const user = await this.userService.findByEmail(credentials.email);
+      const admin = await this.adminService.findByEmail(credentials.email);
+      const gardener = await this.gardenerService.findByEmail(
+        credentials.email,
+      );
 
-            if(user?.isBanned === true) {
-                throw new HttpException('El usuario esta baneado', HttpStatus.UNAUTHORIZED)
-            }
+      if (user?.isBanned === true) {
+        throw new HttpException(
+          'El usuario esta baneado',
+          HttpStatus.UNAUTHORIZED,
+        );
+      }
 
-            // Validación para usuarios
-            if (user) {
-                const isPasswordMatching = await bcrypt.compare(credentials.password, user.password);
-                if (!isPasswordMatching) {
-                    throw new HttpException('Credenciales incorrectas', HttpStatus.UNAUTHORIZED);
-                }
-                const token = await this.createToken(user);
-                return { token, user };
-            }
-
-            // Validación para administradores
-            if (admin) {
-                const isPasswordMatching = await bcrypt.compare(credentials.password, admin.password);
-                if (!isPasswordMatching) {
-                    throw new HttpException('Credenciales incorrectas', HttpStatus.UNAUTHORIZED);
-                }
-                const token = await this.createToken(admin);
-                return { token, user: admin };
-            }
-
-            // Validación para jardineros
-            if (gardener) {
-                const isPasswordMatching = await bcrypt.compare(credentials.password, gardener.password);
-                if (!isPasswordMatching) {
-                    throw new HttpException('Credenciales incorrectas', HttpStatus.UNAUTHORIZED);
-                }
-                const token = await this.createToken(gardener);
-                return { token, user: gardener };
-            }
-
-            throw new HttpException('Usuario no encontrado en la base de datos', HttpStatus.UNAUTHORIZED);
-        } catch (error) {
-            throw new HttpException(error, 401);
+      // Validación para usuarios
+      if (user) {
+        const isPasswordMatching = await bcrypt.compare(
+          credentials.password,
+          user.password,
+        );
+        if (!isPasswordMatching) {
+          throw new HttpException(
+            'Credenciales incorrectas',
+            HttpStatus.UNAUTHORIZED,
+          );
         }
+        const token = await this.createToken(user);
+        return { token, user };
+      }
+
+      // Validación para administradores
+      if (admin) {
+        const isPasswordMatching = await bcrypt.compare(
+          credentials.password,
+          admin.password,
+        );
+        if (!isPasswordMatching) {
+          throw new HttpException(
+            'Credenciales incorrectas',
+            HttpStatus.UNAUTHORIZED,
+          );
+        }
+        const token = await this.createToken(admin);
+        return { token, user: admin };
+      }
+
+      // Validación para jardineros
+      if (gardener) {
+        const isPasswordMatching = await bcrypt.compare(
+          credentials.password,
+          gardener.password,
+        );
+        if (!isPasswordMatching) {
+          throw new HttpException(
+            'Credenciales incorrectas',
+            HttpStatus.UNAUTHORIZED,
+          );
+        }
+        const token = await this.createToken(gardener);
+        return { token, user: gardener };
+      }
+
+      throw new HttpException(
+        'Usuario no encontrado en la base de datos',
+        HttpStatus.UNAUTHORIZED,
+      );
+    } catch (error) {
+      throw new HttpException(error, 401);
+    }
+  }
+
+  async signUp(signUpUser: SignUpAuthDto) {
+    const userFinded = await this.userService.findByEmail(signUpUser.email);
+    const gardenerFinded = await this.gardenerService.findByEmail(
+      signUpUser.email,
+    );
+
+    if (userFinded || gardenerFinded) {
+      throw new BadRequestException('User already exists');
     }
 
-    async signUp(signUpUser: SignUpAuthDto) {
-        const userFinded = await this.userService.findByEmail(signUpUser.email);
-        const gardenerFinded = await this.gardenerService.findByEmail(signUpUser.email);
-
-            if (userFinded) {
-                throw new BadRequestException('User already exists')
-            }
-
-            if (signUpUser.password !== signUpUser.passwordConfirm) {
-                throw new HttpException('Password does not match', 400)
-            }
-
-            signUpUser.password = await bcrypt.hash(signUpUser.password, 10)
-
-            if (!signUpUser.password) {
-                throw new BadRequestException('Error at password hash')
-            }
-
-<<<<<<< HEAD
-
-        const newUser = await this.userService.create(signUpUser)
-            
-        if (userFinded || gardenerFinded) {
-            throw new BadRequestException('El usuario ya existe');
-=======
-            const newUser = await this.userService.create(signUpUser)
-            await this.mailService.sendWelcomeEmail(newUser.email, newUser.username);
-            return newUser;
->>>>>>> 5629a5329a662a1d895d34dc854da596e1fb750c
-        }
-
-        if (signUpUser.password !== signUpUser.passwordConfirm) {
-            throw new HttpException('Las contraseñas no coinciden', HttpStatus.BAD_REQUEST);
-        }
-
-        signUpUser.password = await bcrypt.hash(signUpUser.password, 10);
-        if (!signUpUser.password) {
-            throw new BadRequestException('Error al encriptar la contraseña');
-        }
-
-        let newUsers;
-
-        if (signUpUser.role === Role.Gardener) {
-            newUsers = await this.gardenerService.create(signUpUser);
-        } else {
-            newUsers = await this.userService.create(signUpUser);
-        }
-
-        return newUser;
+    if (signUpUser.password !== signUpUser.passwordConfirm) {
+      throw new HttpException('Password does not match', 400);
     }
 
-    private async createToken(user: User) {
-        const payload = {
-            id: user.id,
-            email: user.email,
-            roles: user.role,
-        };
+    signUpUser.password = await bcrypt.hash(signUpUser.password, 10);
 
-        return this.jwtService.signAsync(payload);
+    if (!signUpUser.password) {
+      throw new BadRequestException('Error at password hash');
     }
+
+    let newUsers;
+
+    if (signUpUser.role === Role.Gardener) {
+      newUsers = await this.gardenerService.create(signUpUser);
+    } else {
+      newUsers = await this.userService.create(signUpUser);
+    }
+
+    const newUser = await this.userService.create(signUpUser);
+    await this.mailService.sendWelcomeEmail(newUser.email, newUser.username);
+    return newUser;
+  }
+
+  private async createToken(user: User) {
+    const payload = {
+      id: user.id,
+      email: user.email,
+      roles: user.role,
+    };
+
+    return this.jwtService.signAsync(payload);
+  }
 }
