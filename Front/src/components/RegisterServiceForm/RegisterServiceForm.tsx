97b29e54dd7a998eeba139b050ad926861810c8f
Pablo <olbap_olimpo@hotmail.com>
2024-11-18 15:27:08 -0300

    Editado navbar y footer para mejor estetica.

--- Front/src/components/RegisterServiceForm/RegisterServiceForm.tsx
+++ Front/src/components/RegisterServiceForm/RegisterServiceForm.tsx
@@ -72,7 +72,7 @@ export default function RegisterServiceForm() {
     // Si no hay errores, enviamos el servicio
     if (Object.keys(validationErrors).length === 0) {
       await registerService(dataService);
-      alert("Service added successfully");
+      alert("Servicio agregado con éxito");
       router.push("/Home"); // Redirigir a la lista de servicios
     }
   };
@@ -87,7 +87,7 @@ export default function RegisterServiceForm() {
 
   return (
     <div className="w-full max-w-md mx-auto mt-24 p-6 border rounded-lg shadow-lg bg-white">
-      <h2 className="text-2xl font-bold text-center mb-4">Agregar un Servicio de Jardineria</h2>
+      <h2 className="text-2xl font-bold text-center mb-4 text-green-700">Agregar un Servicio de Jardinería</h2>
       <form onSubmit={handleSubmit} className="space-y-4">
         <div>
           <label htmlFor="detailService" className="block text-sm font-medium text-gray-700">Detalle del Servicio</label>
@@ -98,7 +98,7 @@ export default function RegisterServiceForm() {
             value={dataService.detailService}
             onChange={handleChange}
             placeholder="Por ej., cortar el césped"
-            className="mt-1 p-2 border border-gray-300 rounded w-full"
+            className="mt-1 p-2 border border-gray-300 rounded w-full focus:ring-2 focus:ring-green-700"
           />
           {touched.detailService && errors.detailService && (
             <span className="text-red-500 text-sm">{errors.detailService}</span>
@@ -116,7 +116,7 @@ export default function RegisterServiceForm() {
             value={dataService.price}
             onChange={handleChange}
             placeholder="50.00"
-            className="mt-1 p-2 border border-gray-300 rounded w-full"
+            className="mt-1 p-2 border border-gray-300 rounded w-full focus:ring-2 focus:ring-green-700"
           />
           {touched.price && errors.price && (
             <span className="text-red-500 text-sm">{errors.price}</span>
@@ -124,7 +124,7 @@ export default function RegisterServiceForm() {
         </div>
 
         <div>
-          <label className="block text-sm font-medium text-gray-700">Categorias</label>
+          <label className="block text-sm font-medium text-gray-700">Categorías</label>
           <div className="mt-2 space-y-2">
             {categories.map((category) => (
               <div key={category} className="flex items-center">
@@ -134,7 +134,7 @@ export default function RegisterServiceForm() {
                   type="checkbox"
                   value={category}
                   onChange={handleCategoriesChange}
-                  className="h-4 w-4 text-indigo-600 border-gray-300 rounded"
+                  className="h-4 w-4 text-green-700 border-gray-300 rounded"
                 />
                 <label
                   htmlFor={category}
@@ -150,7 +150,11 @@ export default function RegisterServiceForm() {
           )}
         </div>
 
-        <button type="submit" disabled={Object.values(errors).some(error => error !== '')} className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center">
+        <button
+          type="submit"
+          disabled={Object.values(errors).some((error) => error !== '')}
+          className="text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center"
+        >
           Agregar Servicio
         </button>
       </form>
--- Front/src/components/SobreNosotrosCompo/SobreNosotrosCompo.tsx
+++ Front/src/components/SobreNosotrosCompo/SobreNosotrosCompo.tsx
@@ -1,102 +1,92 @@
-import React from 'react'
-
+import React from 'react';
 import Image from "next/image";
-// import { Playfair_Display, Roboto, Lora } from '@next/font/google';
-
-// const playfairDisplay = Playfair_Display({
-//   subsets: ['latin'],
-//   variable: '--font-playfair',
-// });
-
-// const roboto = Roboto({
-//   subsets: ['latin'],
-//   variable: '--font-roboto',
-//   weight: ['400', '700'],
-// });
-
-// const lora = Lora({
-//   subsets: ['latin'],
-//   variable: '--font-lora',
-// });
-
 
 const SobreNosotrosCompo = () => {
   return (
+    <div>
+      <main className="flex min-h-screen flex-col items-center justify-between py-8 mt-16 text-center sm:p-24 sm:text-justify">
+        {/* Contenedor principal */}
+        <div className="mx-auto bg-white p-4 shadow-lg grid grid-cols-1 gap-4 my-2 sm:p-8 lg:max-w-4xl">
+          {/* Título */}
+          <h1 className="text-3xl font-extrabold mb-6 text-[#263238] font-playfair sm:text-4xl lg:text-5xl">
+            ¿Quiénes somos?
+          </h1>
 
-   <div>
-      <main className={`flex min-h-screen flex-col items-center justify-between py-8 mt-16 text-center sm:p-24 sm:text-justify`}>
-        <div className="mx-auto bg-white p-1 shadow-lg grid grid-cols-1 gap-4 my-2 sm:p-8 lg:max-w-4xl">
-          <h1 className="text-xl font-bold mb-1 font-playfair sm:text-2xl lg:text-4xl lg:mb-6">¿Quiénes somos?</h1>
-          <p className="mb-4 font-lora text-sm sm:text-base">
-          <strong>Vicnasol</strong> es una empresa de servicios ambientales con más de 30 años de experiencia, ubicada en Carrasco, Montevideo y Ciudad de la Costa, Uruguay. Estamos especializados en todos los trabajos de jardinería, ofreciendo soluciones personalizadas para satisfacer las necesidades de particulares y empresas que buscan un servicio de calidad. Nuestro equipo de 10 profesionales altamente cualificados le asesorará y guiará en cada etapa de su proyecto.
+          {/* Descripción */}
+          <p className="mb-6 font-lora text-sm sm:text-base text-[#263238]">
+            <strong>Vicnasol</strong> es una empresa de servicios ambientales con más de 30 años de experiencia, ubicada en Carrasco, Montevideo y Ciudad de la Costa, Uruguay. Estamos especializados en todos los trabajos de jardinería, ofreciendo soluciones personalizadas para satisfacer las necesidades de particulares y empresas que buscan un servicio de calidad. Nuestro equipo de 10 profesionales altamente cualificados le asesorará y guiará en cada etapa de su proyecto.
           </p>
 
-          <div className="relative bg-gray-200 p-6 pl-10 mb-6 border-l-8 border-green-700 font-lora">
-            <span className="absolute left-0 top-0 text-6xl text-green-700 leading-none font-playfair">
+          {/* Cita destacada */}
+          <div className="relative bg-[#F1F8E9] p-6 pl-10 mb-6 border-l-8 border-[#388E3C] font-lora text-[#263238]">
+            <span className="absolute left-0 top-0 text-6xl text-[#388E3C] leading-none font-playfair">
               “
             </span>
             <p className="text-lg italic">
-            En Vicnasol damos vida a tus ideas. Desde la primera reunión hasta la finalización de cada proyecto, nos dedicamos a garantizar que no se pase por alto ni un solo detalle.
+              En Vicnasol damos vida a tus ideas. Desde la primera reunión hasta la finalización de cada proyecto, nos dedicamos a garantizar que no se pase por alto ni un solo detalle.
             </p>
-            <span className="absolute right-0 bottom-0 text-6xl text-green-700 leading-none font-playfair">
+            <span className="absolute right-0 bottom-0 text-6xl text-[#388E3C] leading-none font-playfair">
               ”
             </span>
           </div>
 
-          <p className="mb-4 font-lora text-sm sm:text-base">
-          En <strong>Vicnasol</strong> ofrecemos una amplia gama de servicios de mantenimiento de jardines para empresas, comunidades residenciales y particulares, así como mantenimiento de plantas de interior para centros comerciales, oficinas y hoteles, potenciando la imagen corporativa de cara a los clientes.
+          {/* Más detalles sobre los servicios */}
+          <p className="mb-4 font-lora text-sm sm:text-base text-[#263238]">
+            En <strong>Vicnasol</strong> ofrecemos una amplia gama de servicios de mantenimiento de jardines para empresas, comunidades residenciales y particulares, así como mantenimiento de plantas de interior para centros comerciales, oficinas y hoteles, potenciando la imagen corporativa de cara a los clientes.
           </p>
 
-          <ul className="list-disc pl-6 mb-4 font-lora text-justify text-sm sm:text-base">
-            <li>
-            Desmonte y limpieza de parcelas, taludes, zonas industriales, etc.
-            </li>
-            <li>
-            Poda y tala de árboles mediante técnicas trepadoras y/o camiones grúa, realizadas por profesionales capacitados.
-            </li>
+          {/* Lista de servicios */}
+          <ul className="list-disc pl-6 mb-4 font-lora text-justify text-sm sm:text-base text-[#263238]">
+            <li>Desmonte y limpieza de parcelas, taludes, zonas industriales, etc.</li>
+            <li>Poda y tala de árboles mediante técnicas trepadoras y/o camiones grúa, realizadas por profesionales capacitados.</li>
             <li>Instalación y reparación de sistemas de riego automático.</li>
             <li>Tratamientos fitosanitarios.</li>
             <li>Control de plagas de hortalizas.</li>
             <li>Proyectos de jardinería.</li>
-            </ul>
+          </ul>
 
-          <p className="font-lora text-sm sm:text-base">
-          En <strong>Vicnasol</strong> nuestro objetivo es dar un servicio integral y de calidad, garantizando la satisfacción del cliente y el cuidado del medio ambiente. Estamos orgullosos de nuestra trayectoria y de la confianza que nuestros clientes han depositado en nosotros a lo largo de los años.
+          {/* Descripción final */}
+          <p className="font-lora text-sm sm:text-base text-[#263238]">
+            En <strong>Vicnasol</strong> nuestro objetivo es dar un servicio integral y de calidad, garantizando la satisfacción del cliente y el cuidado del medio ambiente. Estamos orgullosos de nuestra trayectoria y de la confianza que nuestros clientes han depositado en nosotros a lo largo de los años.
           </p>
+
+          {/* Botón */}
+          <div className="text-center mt-6">
+            <a href="/contacto">
+              <button className="bg-[#4CAF50] text-white py-2 px-6 rounded-md hover:bg-[#388E3C] transition duration-200 font-bold">
+                Contáctanos
+              </button>
+            </a>
+          </div>
         </div>
 
-      <div className="grid grid-cols-1 gap-4 mx-4 my-4 sm:grid-cols-2 lg:grid-cols-3">
-        <Image
-          src="/images/trabajador.jpg"
-          alt="Trabajador"
-          width={1800}
-          height={920}
-          className="sm:min-h-full hover:transform hover:scale-105 transition duration-200 rounded-md"
-        />
-
-        <Image
-          src="/images/entrada.jpg"
-          alt="Trabajador"
-          width={1800}
-          height={920}
-          className="hover:transform hover:scale-105 transition duration-200 rounded-md"
-        />
-
-        <Image
-          src="/images/rosas.jpg"
-          alt="Trabajador"
-          width={1800}
-          height={920}
-          className="hover:transform hover:scale-105 transition duration-200 rounded-md"
-        />
-      </div>
-    </main>
+        {/* Galería de imágenes */}
+        <div className="grid grid-cols-1 gap-4 mx-4 my-4 sm:grid-cols-2 lg:grid-cols-3">
+          <Image
+            src="/images/trabajador.jpg"
+            alt="Trabajador"
+            width={1800}
+            height={920}
+            className="hover:transform hover:scale-105 transition duration-200 rounded-md"
+          />
+          <Image
+            src="/images/entrada.jpg"
+            alt="Entrada"
+            width={1800}
+            height={920}
+            className="hover:transform hover:scale-105 transition duration-200 rounded-md"
+          />
+          <Image
+            src="/images/rosas.jpg"
+            alt="Rosal"
+            width={1800}
+            height={920}
+            className="hover:transform hover:scale-105 transition duration-200 rounded-md"
+          />
+        </div>
+      </main>
     </div>
-  )
-}
+  );
+};
 
-export default SobreNosotrosCompo
-
-
-
-
+export default SobreNosotrosCompo;
