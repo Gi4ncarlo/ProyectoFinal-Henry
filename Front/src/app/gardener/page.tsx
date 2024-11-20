0e78d07a61b09c8ef7e92afe29b58101beb3ed03
Negritoyunou <nachoarevalo11@gmail.com>
2024-11-20 10:06:47 -0300

    Deployando v2.3

--- Front/src/app/gardener/page.tsx
+++ Front/src/app/gardener/page.tsx
@@ -1,17 +1,9 @@
 import ProviderCardList from "@/components/ProviderCardList/ProviderCardList";
 import React from "react";
-import dynamic from 'next/dynamic'
  
-const DynamicComponentWithNoSSR = dynamic(
-  () => import('../../app/gardener/[id]/page'),
-  { ssr: false }
-)
-
-
 const gardeners = () => {
   return (
     <div className="flex">
-      <DynamicComponentWithNoSSR/>
       <ProviderCardList />
     </div>
   );
