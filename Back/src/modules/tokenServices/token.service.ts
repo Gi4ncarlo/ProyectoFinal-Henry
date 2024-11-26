62fa27aeea8f1a2e7423bb7b23c7a16c8ab3adb6
lucasbadino <lucas.badino.7@gmail.com>
2024-11-26 15:25:19 -0300

    add validator token by gardener

--- Back/src/modules/tokenServices/token.service.ts
+++ Back/src/modules/tokenServices/token.service.ts
@@ -1,18 +1,40 @@
-import { Injectable } from "@nestjs/common";
+import { HttpException, Injectable } from "@nestjs/common";
+import { InjectRepository } from "@nestjs/typeorm";
 import { nanoid } from "nanoid";
+import { ServicesOrderEntity } from "../services-order/entities/services-order.entity";
+import { Repository } from "typeorm";
+import { Status } from "../service-details/enum/status.enum";
+import { ServiceDetail } from "../service-details/entities/service-detail.entity";
 
 
 @Injectable()
 export class TokenService {
-    constructor() { }
+    constructor(
+        @InjectRepository(ServicesOrderEntity)
+        private readonly orderService: Repository<ServicesOrderEntity>,
+        @InjectRepository(ServiceDetail)
+        private readonly serviceDetail: Repository<ServiceDetail>,
+    ) { }
     async generateToken(num: number = 6): Promise<string> {
         return await nanoid(num);
     }
 
-    async checkToken(userToken: string, gardenerToken: string): Promise<boolean> {
-        if (userToken === gardenerToken) {
-            return true;
-        }
-        return false;
+    async checkToken(body, id: string): Promise<boolean> {
+        const order = await this.orderService.findOne(
+            {
+                where: { id },
+                relations: ['orderDetail'],
+            },
+        );
+        console.log(order);
+        if (!order) throw new HttpException('No se encontro la orden de servicio', 404);
+        if (order.orderDetail.userToken !== body.token) throw new HttpException('El token es incorrecto', 401);
+        const detail = await this.serviceDetail.findOneBy({ id: order.orderDetail.id });
+        detail.status = Status.Finished;
+        detail.gardenerToken = body.token;
+        await this.serviceDetail.save(detail);
+        return true;
+
     }
+
 }
--- Front/src/components/DashboardGardenerCompo/orders/modal.tsx
+++ Front/src/components/DashboardGardenerCompo/orders/modal.tsx
@@ -1,15 +1,44 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 interface ModalDetailsProps {
     order: any;
     onClose: () => void;
+
 }
 
 const ModalDetails: React.FC<ModalDetailsProps> = ({ order, onClose }) => {
     const [token, setToken] = useState("");
+    const [userToken, setUserToken] = useState("");
 
+
+
+    useEffect(() => {
+        if (typeof window !== "undefined" && window.localStorage) {
+            const storedSession = JSON.parse(
+                localStorage.getItem("userSession") || ""
+            );
+            setUserToken(storedSession.token);
+        }
+    }, []);
     const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
         setToken(event.target.value);
+    };
+    const handleOnClick = async (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+        console.log(token);
+
+        const response = await fetch(
+            `${process.env.NEXT_PUBLIC_API_URL}/token/check/${order.id}`,
+            {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                    Authorization: `Bearer ${userToken}`,
+                },
+                body: JSON.stringify({ token }),
+            }
+        )
+        const data = await (await response).json();
+        console.log(data);
     };
 
     if (!order) return null;
@@ -69,30 +98,53 @@ const ModalDetails: React.FC<ModalDetailsProps> = ({ order, onClose }) => {
                         <strong>Dirección:</strong> {order.user.address || "No proporcionada"}
                     </p>
                 </div>
-
-                {/* Campo de entrada del token */}
-                <div className="mb-6">
-                    <label className="block text-gray-700 font-medium mb-2" htmlFor="token">
-                        Ingrese un token
-                    </label>
-                    <input
-                        type="text"
-                        id="token"
-                        maxLength={6}
-                        value={token}
-                        onChange={handleInputChange}
-                        placeholder="Escriba el token aquí"
-                        className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-400 focus:outline-none"
-                    />
+                <div className="mb-6 p-4 border border-gray-200 rounded-lg bg-blue-50 shadow-inner space-y-3">
+                    <h3 className="text-lg font-semibold text-blue-700">Status del servicio</h3>
+                    <p className="text-sm text-gray-700">
+                        <strong>Estado:</strong> {order.orderDetail.status}
+                    </p>
                 </div>
 
-                {/* Botón de acción */}
-                <button
-                    onClick={() => console.log("Token ingresado:", token)}
-                    className="w-full bg-blue-600 text-white py-2 px-4 rounded-lg hover:bg-blue-700 transition duration-200"
-                >
-                    Confirmar
-                </button>
+                {/* Campo de entrada del token */}
+                <>{order.orderDetail.status === 'Finalizado' ?
+                    <div className="mt-8 bg-gray-800 text-white p-6 rounded-lg shadow">
+                        <label htmlFor="token" className="block text-sm font-medium mb-2 text-center">Felicidades El Token es Valido</label>
+                        <input
+                            type="text"
+                            id="token"
+                            value={order.orderDetail.status}
+                            readOnly
+                            className="w-full bg-gray-700 text-center text-gray-200 p-3 rounded-md border border-gray-600 focus:outline-none focus:ring-2 focus:ring-gray-400"
+                        />
+                    </div>
+                    :
+                    (<>
+                        <div className="mb-6">
+                            <label className="block text-gray-700 font-medium mb-2" htmlFor="token">
+                                Ingrese un token
+                            </label>
+                            <input
+                                type="text"
+                                id="token"
+                                maxLength={6}
+                                value={token}
+                                onChange={handleInputChange}
+                                placeholder="Escriba el token aquí"
+                                className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-400 focus:outline-none"
+                            />
+                        </div>
+
+                        {/* Botón de acción */}
+                        <button
+                            onClick={handleOnClick}
+                            className="w-full bg-blue-600 text-white py-2 px-4 rounded-lg hover:bg-blue-700 transition duration-200"
+                        >
+                            Confirmar
+                        </button>
+                    </>
+                    )}
+
+                </>
             </div>
         </div>
     );
