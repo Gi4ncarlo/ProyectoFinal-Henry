1c22bbb1c4cc86a4d5835b687b1676ec4055ae7f
Giancarlo <giancarlozanarini9@gmail.com>
2024-11-27 16:13:54 -0300

    Comento registerServiceForm ya que no se usa

--- Front/src/app/registerService/page.tsx
+++ Front/src/app/registerService/page.tsx
@@ -1,9 +1,9 @@
-import RegisterServiceForm from "@/components/RegisterServiceForm/RegisterServiceForm";
+//import RegisterServiceForm from "../../components/RegisterServiceForm/RegisterServiceForm";
 
 export default function RegisterServicePage() {
   return (
     <div className="min-h-screen flex items-center justify-center">
-      <RegisterServiceForm />
+      {/* <RegisterServiceForm /> */}
     </div>
   );
 }
